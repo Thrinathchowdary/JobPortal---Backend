b03047f945f09733f800d59f442de1bad50dca78
Thrinath Chowdary <3nathchowdary23@gmail.com>
2026-02-05 16:22:02 +0530

    Fix MySQL SSL connection

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: true,
+    rejectUnauthorized: false,
   },
   waitForConnections: true,
   connectionLimit: 10,
